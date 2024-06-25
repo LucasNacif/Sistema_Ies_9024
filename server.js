37cfda38b170ea7f01c6803f97f410622832dc42
MircoSantoni <santonimircoariel@gmail.com>
2024-06-25 19:34:14 -0300

    Mongo Db Update
    This is commit with the persistence upgrade.

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const Handlebars = require("handlebars");
 const fs = require("fs");
-const Seguridad = require("./models/seguridad.js");
-const app = express();
+//const { obtenerAlumnos } = require("./models/modelo.js");
+//const Seguridad = require("./models/seguridad.js");
 const userRouters = require("./App/routes/user");
-const { obtenerAlumnos } = require("./models/modelo.js");
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const port = 3000;
+
 
 app.use(express.static("public"));
 
@@ -25,6 +28,18 @@ _url = "http://localhost:" + port;
 //var objeto = {url : _url+"/login"};
 var objeto = { url: _url };
 let destino = { url: "" };
+
+
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Conectado a MongoDB Atlas'))
+.catch((err) => console.error('Error al conectar a MongoDB Atlas:', err));
+
+
+
 //------------- zona de ruteo ------------------
 app.get("/", (req, res) => {
   var archivo = fs.readFileSync("./views/index.hbs", "utf-8", (err, data) => {
@@ -38,6 +53,7 @@ app.get("/", (req, res) => {
   var salida = template(objeto);
   res.send(salida);
 });
+
 
 app.get("/nuevo", (req, res) => {
   console.log("llegó un post/nuevo");
@@ -54,31 +70,6 @@ app.get("/nuevo", (req, res) => {
   res.send(salida);
 });
 
-app.post("/login", (req, res) => {
-  console.log("browser --> server 'post/login'");
-  console.log("server --> seguridad 'registrado(req.body)'");
-
-  let registrado = Seguridad.registrado(req.body);
-
-  if (registrado == true) {
-    console.log("server <-r- seguridad 'true'");
-    var archivo = fs.readFileSync("./views/menu.hbs", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        //console.log("archivo leído");
-      }
-    });
-    var template = Handlebars.compile(archivo);
-    var salida = template(objeto);
-    console.log("browser <-r- server 'menu.html'");
-    res.send(salida);
-  } else {
-    console.log("server <-r- seguridad 'false'");
-    console.log("browser <-r- server 'Error...!!!.html'");
-    res.send("<p>Error...!!!</p>");
-  }
-});
 
 //Redirije al menu
 app.get("/menu", (req, res) => {
@@ -96,25 +87,33 @@ app.get("/menu", (req, res) => {
 
 app.use(userRouters);
 
-// //ejecuta el metodo obtenerAlumnos
-// app.get("/alumnos", (req, res) => {
-//   Alumnos.obtenerAlumnosTxt(req, res);
-// });
-
-// //redirije a listar
-// app.get("/listar", (req, res) => {
-//   var archivo = fs.readFileSync("./views/listar.hbs", "utf-8", (err, data) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("archivo leído");
-//     }
-//   });
-//   var template = Handlebars.compile(archivo);
-//   var salida = template(objeto);
-//   res.send(salida);
-// });
 
 app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
 });
+
+    // app.post("/login", (req, res) => {
+    //   console.log("browser --> server 'post/login'");
+    //   console.log("server --> seguridad 'registrado(req.body)'");
+    
+    //   let registrado = Seguridad.registrado(req.body);
+    
+    //   if (registrado == true) {
+    //     console.log("server <-r- seguridad 'true'");
+    //     var archivo = fs.readFileSync("./views/menu.hbs", "utf-8", (err, data) => {
+    //       if (err) {
+    //         console.log(err);
+    //       } else {
+    //         //console.log("archivo leído");
+    //       }
+    //     });
+    //     var template = Handlebars.compile(archivo);
+    //     var salida = template(objeto);
+    //     console.log("browser <-r- server 'menu.html'");
+    //     res.send(salida);
+    //   } else {
+    //     console.log("server <-r- seguridad 'false'");
+    //     console.log("browser <-r- server 'Error...!!!.html'");
+    //     res.send("<p>Error...!!!</p>");
+    //   }
+    // });
