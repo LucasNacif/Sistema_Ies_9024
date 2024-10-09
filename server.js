27bff58c318c637ca20f4c7ef5b5095e9c0d82c8
Matías Garay <matymgcgaray@gmail.com>
2024-10-09 18:40:03 -0300

    crud en mesas y vistas de alumno

--- server.js
+++ server.js
@@ -40,13 +40,14 @@ app.use(carreraRouters);
 app.use(alumnoRouters);
 app.use(loginRouters);
 
-app.get("/", (req, res) => res.render("Admin_PanelControl"));
+// app.get("/", (req, res) => res.render("Admin_PanelControl"));
 app.get("/index", (req, res) => res.render("index"));
 app.get("/alumno", (req, res) => res.render("Admin_Alumno"));
 app.get("/mesa", (req, res) => res.render("Admin_Mesa"));
 app.get("/materia", (req, res) => res.render("Admin_Materia"));
 app.get("/Administracion", (req, res) => res.render("Admin_PanelControl"));
 app.get("/AdministracionSuperAdmin", (req, res) => res.render("SuperAdmin_PanelControl"));
+app.get("/", (req, res) => res.render("Alumno_Mesa"));
 
 //NO BORRAR QUE ME COSTO UN HUEVO HACERLO :)
 
