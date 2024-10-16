794535447149cda9ceabff8f83fee8db5c113ca1
Steven Gimenez <stevengimenez1@gmail.com>
2024-10-16 02:57:42 -0300

    Features/TablesOnHTML+

--- App/routes/R_Alumno.js
+++ App/routes/R_Alumno.js
@@ -7,6 +7,6 @@ router.post("/alumno/agregar", controller.nuevo);
 router.get("/alumno/traerPorDoc/:numDocAlumn", controller.traerPorDoc);
 router.delete("/alumno/baja/:numDocAlumn", controller.darDeBaja);
 router.post("/alumno/modificar", controller.modificarAlumno);
-router.get('/alumno/obtenerAlumnos', controller.obtenerAlumnosActivos);
+router.get('/alumno/obtener', controller.obtenerAlumnosActivos);
 
 module.exports = router;
