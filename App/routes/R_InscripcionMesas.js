c8adaf675f40b39e58e64a0d26331efccc0b63d3
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-10-16 21:07:05 -0300

    Features/metodo para mostrar las mesas correspondientes por carrera

--- App/routes/R_InscripcionMesas.js
+++ App/routes/R_InscripcionMesas.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const controller = require("../controllers/C_inscripcionMesa");
 
 //RUTAS PARA ALUMNOS LOGUEADOS
+
 router.get('/inscripcion/obtenerMesasActivas/', controller.obtenerMesasActivas);
-router.get('/inscripcion/alumno/:alumnoId/mesa/:mesaId/', controller.verificarPermisoParaRendir);
+router.get('/inscripcion/obtenerMesasSegunAlum/:numDoc', controller.obtenerMesasSegunAlum);
+router.post('/inscripcion/alumno', controller.verificarPeermisoParaRendir);
+
+// router.get('/inscripcion/alumno/:alumnoId/mesa/:mesaId/', controller.verificarPermisoParaRendir);
 
 module.exports = router;
