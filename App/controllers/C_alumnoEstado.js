bfa078a381fcf620e6d847aa5b2a28e339e68bd7
MatiCastillo145 <maticastillo145@gmail.com>
2024-10-31 22:31:07 -0300

    Modificacion de metodo crearAlumnoEstado en C_alumnoEstado

--- App/controllers/C_alumnoEstado.js
+++ App/controllers/C_alumnoEstado.js
@@ -8,32 +8,41 @@ exports.crearAlumnoEstado = async (req, res) => {
   try {
     const { nombreAlumno, nombreMateria, estadoActual } = req.body;
 
-    //console.log("Datos recibidos:", { nombreAlumno, nombreMateria });
-
+    // Buscar el alumno
     const alumno = await Alumno.findOne({ nombreCompleto: nombreAlumno });
     if (!alumno) {
-      console.log("Alumno no encontrada");
-      throw new Error("Alumno no encontrado");
+      console.log("Alumno no encontrado");
+      return res.status(404).json({ message: "Alumno no encontrado" });
     }
-    // Buscar la materia por nombre
+
+    // Buscar la materia
     const materia = await Materia.findOne({ nombreMateria: nombreMateria });
     if (!materia) {
       console.log("Materia no encontrada");
-      throw new Error("Materia no encontrada");
+      return res.status(404).json({ message: "Materia no encontrada" });
     }
 
-    const nuevoAlumnoEstado = new AlumnoEstado({
-      idAlumno: alumno._id,
-      idMateria: materia._id,
-      historialEstados: [{ estado: estadoActual, fecha: new Date() }]
-    });
+    // Buscar si ya existe un estado para el alumno y la materia
+    let alumnoEstado = await AlumnoEstado.findOne({ idAlumno: alumno._id, idMateria: materia._id });
 
-    await nuevoAlumnoEstado.save();
-
-    res.status(201).json({ message: "AlumnoEstado creado exitosamente" });
+    if (alumnoEstado) {
+      // Si existe, añadir el nuevo estado al historial
+      alumnoEstado.historialEstados.push({ estado: estadoActual, fecha: new Date() });
+      await alumnoEstado.save();
+      res.status(200).json({ message: "Estado actualizado exitosamente" });
+    } else {
+      // Si no existe, crear un nuevo AlumnoEstado
+      const nuevoAlumnoEstado = new AlumnoEstado({
+        idAlumno: alumno._id,
+        idMateria: materia._id,
+        historialEstados: [{ estado: estadoActual, fecha: new Date() }]
+      });
+      await nuevoAlumnoEstado.save();
+      res.status(201).json({ message: "AlumnoEstado creado exitosamente" });
+    }
   } catch (error) {
     console.log(error.message);
-    res.status(501).json({ message: "Error al crear el AlumnoEstado: ${error.message}" });
+    res.status(500).json({ message: `Error al crear o actualizar el AlumnoEstado: ${error.message}` });
   }
 };
 
