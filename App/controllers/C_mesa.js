6e79e072175a16427e0342a3b4bea1a6b468646b
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-11-07 21:46:40 -0300

    Fix/Modifcar mesa, estilos varios

--- App/controllers/C_mesa.js
+++ App/controllers/C_mesa.js
@@ -7,18 +7,15 @@ exports.crearMesa = async (req, res) => {
         const {
             fechaMesa,
             horaMesa,
-            Materia,
-            Alumno,
-            estadoActual
+            Materia
         } = req.body;
 
-        console.log(req.body)
         const nuevaMesa = new Mesa({
             fechaMesa,
             horaMesa,
             Materia,
-            Alumno: Alumno || [],
-            estadoActual: estadoActual || 'activa'
+            Alumno: [],
+            estadoActual: 'activa'
         });
 
         await nuevaMesa.save();
@@ -32,66 +29,35 @@ exports.crearMesa = async (req, res) => {
             success: false,
             message: "Error al crear la mesa"
         });
-    }
+    }
 };
 // Modificar Mesa
 exports.modificarMesa = async (req, res) => {
     try {
         const { id } = req.params;
         const { fechaMesa, horaMesa, Materia, estadoActual } = req.body;
-        
+
         const mesaModificada = await Mesa.findByIdAndUpdate(
             id,
             { fechaMesa, horaMesa, Materia, estadoActual },
             { new: true }
         );
-        
+
         if (!mesaModificada) return res.status(404).json({ message: "Mesa no encontrada" });
-        
+
         res.status(200).json({ message: "Mesa actualizada correctamente" });
     } catch (error) {
         console.error(error);
         res.status(400).json({ message: "Error al modificar la mesa" });
     }
 };
-
-// Dar de baja una Mesa
-exports.darDeBajaMesa = async (req, res) => {
-    try {
-        const { id } = req.params;
-        
-        const mesaBaja = await Mesa.findByIdAndUpdate(
-            id,
-            { estadoActual: 'suspendida' },
-            { new: true }
-        );
-        
-        if (!mesaBaja) return res.status(404).json({ message: "Mesa no encontrada" });
-        
-        res.status(200).json({ message: "Mesa dada de baja correctamente" });
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ message: "Error al dar de baja la mesa" });
-    }
-};
-
-// Mostrar mesas suspendidas
-exports.mostrarMesasSuspendidas = async (req, res) => {
-    try {
-        const mesasSuspendidas = await Mesa.find({ estadoActual: 'suspendida' });
-        res.status(200).json(mesasSuspendidas);
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ message: "Error al obtener mesas suspendidas" });
-    }
-};
-
-exports.obtenerMesa = async (req, res) =>{
+//Obtener Mesas
+exports.obtenerMesa = async (req, res) => {
     try {
         const mesasActivas = await Mesa.find().populate("Materia").populate('Alumno');
         const materiasDiponibles = await Materia.find();
-        
-        res.status(200).render("Admin_Mesa", {mesasActivas, materiasDiponibles})
+
+        res.status(200).render("Admin_Mesa", { mesasActivas, materiasDiponibles })
     } catch (error) {
         console.error(error);
         res.status(400).json({ message: "Error al obtener mesas activas" });
--- App/routes/R_Mesa.js
+++ App/routes/R_Mesa.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const controller = require("../controllers/C_mesa");
 
 router.post("/mesa/crear", controller.crearMesa);
+
 router.post("/mesa/modificar/:id", controller.modificarMesa);
-router.put("/mesa/darDeBaja/:id", controller.darDeBajaMesa);
-router.get("/mesa/mostrar/Suspendidas", controller.mostrarMesasSuspendidas);
+
 router.get("/mesa", controller.obtenerMesa)
 
 module.exports = router;
